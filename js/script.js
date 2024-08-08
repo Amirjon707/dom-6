ec047550490e664d7ff55ef5c7ca1c18bce1c11b
Amirjon <amirjonxamdamov2009@gmail.com>
2024-08-08 14:21:46 +0500

    fix

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ const Bars = document.querySelector('.fa-bars')
 const burgerMenu = document.querySelector('.burgerMenu')
 const burgerX = document.querySelector('.burgerX')
 const modalBtn = document.querySelector('.modalBtn')
-const modal = document.querySelector('.blur')
+const modal = document.querySelector('.modal')
+const blurM = document.querySelector('.blur')
 const modalX = document.querySelector('.modalX')
 const send = document.querySelector('.send')
 const inp = document.querySelectorAll('input')
@@ -17,11 +18,15 @@ burgerX.onclick = () => {
 }
 
 modalBtn.onclick = () => {
-    modal.style.transform = `scale(1)`
+    blurM.style.transform = `scale(1)`
+    blurM.style.transitionDelay = `0s`
+    modal.style.transform = `translate(-50%, -50%) scale(1)`
 }
 
 modalX.onclick = () => {
-    modal.style.transform = `scale(0)`
+    modal.style.transform = `translate(-50%, -50%) scale(0)`
+    blurM.style.transitionDelay = `.3s`
+    blurM.style.transform = `scale(0)`
 }
 send.onclick = () => {
     if (inp[0].value == "" || inp[1].value == "") {
@@ -37,7 +42,9 @@ send.onclick = () => {
             inp[1].style.border = '1px solid #3333334d'
         }
     } else {
-        modal.style.transform = `scale(0)`
+        modal.style.transform = `translate(-50%, -50%) scale(0)`
+        blurM.style.transitionDelay = `.3s`
+        blurM.style.transform = `scale(0)`
         inp[0].value = ''
         inp[1].value = ''
         inp[0].style.border = '1px solid #3333334d'
